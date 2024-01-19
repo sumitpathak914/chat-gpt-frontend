dc8b27455ba2540dafa18c694ac16c6c91ad0f03
sumitpathak914 <108678691+sumitpathak914@users.noreply.github.com>
2024-01-19 18:39:49 +0530

    Update App.js
    changing the name

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
     <div className="min-h-screen flex items-center justify-center">
       {!isLoggedIn ? (
         <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-          <h2 className="mb-4 text-xl font-bold">Login  ChatBot</h2>
+          <h2 className="mb-4 text-xl font-bold">Login to ChatBot</h2>
           <input
             className="mb-4 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             type="text"
