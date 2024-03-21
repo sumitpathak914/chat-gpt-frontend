77a262e04a866c8ded0617c8be7f2519c736fdb5
sumitpathak914 <108678691+sumitpathak914@users.noreply.github.com>
2024-03-21 20:59:52 +0530

    adding changes
    Adding changes

---
+++ src/Component/TopNabar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TopNavbar() {
+  return (
+    <div className='bg-black text-white text-center font-serif p-5'>
+        FREE SHIPPING ALL INDIA
+    </div>
+  )
+}
